2b3e5407d14eb8a092a00dd75d2ed20bb861c84d
Damian Buttle <damian.buttle@flexiana.com>
2018-10-17 21:24:54 -0500

    Changed path

--- Components/App/App.js
+++ Components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import '.Components/App/App.css';
+import './App.css';
+import Footer from './Components';
 
 
 class App extends Component {
